b3e7d70f159653562fc4f5f71318f81c4c84a14f
Justin Makaila <justinmakaila@gmail.com>
2017-04-04 11:53:03 -0400

    Initial Commit

---
+++ Lunar/Lunar.h
@@ -0,0 +1,19 @@
+//
+//  Lunar.h
+//  Lunar
+//
+//  Created by Justin Makaila on 4/4/17.
+//  Copyright © 2017 Justin Makaila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lunar.
+FOUNDATION_EXPORT double LunarVersionNumber;
+
+//! Project version string for Lunar.
+FOUNDATION_EXPORT const unsigned char LunarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lunar/PublicHeader.h>
+
+
